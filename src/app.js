09b1f3e2dd4c4095efccc69654337924ab244940
Toby Carlo <tobycarlo.c@gmail.com>
2022-05-09 15:27:19 +0100

    list of emails

--- src/app.js
+++ src/app.js
@@ -1,47 +1,50 @@
-import Header from './components/header'
-
-import initialEmails from './data/emails'
-
-import './styles/app.css'
+import Header from "./components/header";
+import initialEmails from "./data/emails";
+import { useState } from "react";
+import "./styles/app.css";
 
 function App() {
-  // Use initialEmails for state
-  console.log(initialEmails)
+  const [listOfEmails, setEmails] = useState(initialEmails);
+  console.log(initialEmails);
+
+  const emails = listOfEmails.map((renderEmails) => {
+    return (
+      <li className="email">
+        <div className="select">
+          <input className="select-checkbox" type="checkbox" />
+        </div>
+        <div className="star">
+          <input className="star-checkbox" type="checkbox" />
+        </div>
+        <div className="sender">{renderEmails.sender}</div>
+        <div className="title">{renderEmails.title}</div>
+      </li>
+    );
+  });
 
   return (
     <div className="app">
       <Header />
       <nav className="left-menu">
         <ul className="inbox-list">
-          <li
-            className="item active"
-            // onClick={() => {}}
-          >
+          <li className="item active">
             <span className="label">Inbox</span>
             <span className="count">?</span>
           </li>
-          <li
-            className="item"
-            // onClick={() => {}}
-          >
+          <li className="item">
             <span className="label">Starred</span>
             <span className="count">?</span>
           </li>
 
           <li className="item toggle">
             <label for="hide-read">Hide read</label>
-            <input
-              id="hide-read"
-              type="checkbox"
-              checked={false}
-              // onChange={() => {}}
-            />
+            <input id="hide-read" type="checkbox" checked={false} />
           </li>
         </ul>
       </nav>
-      <main className="emails">{/* Render a list of emails here */}</main>
+      <main className="emails">{emails}</main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
